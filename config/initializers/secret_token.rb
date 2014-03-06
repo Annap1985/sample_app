e4d6d8d694080df016798a109af6dc8357660a63
annap1985 <anutayak@gmail.com>
2014-03-06 14:31:49 +0400

    Initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,7 +10,7 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 
-equire 'securerandom'
+require 'securerandom'
 
 def secure_token
   token_file = Rails.root.join('.secret')
